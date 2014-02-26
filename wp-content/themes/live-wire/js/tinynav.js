36252ed2c2fd6a7be20a412a8595d398bf87c6b6
Antônio Roberto Silva <forevertonny@gmail.com>
2014-02-25 21:08:22 -0300

    added liver-wire theme

---
+++ wp-content/themes/live-wire/js/tinynav.js
@@ -0,0 +1,14 @@
+// TinyNav.js
+	jQuery(document).ready(function() {
+    		jQuery('#menu-primary-items').tinyNav({
+			active: 'selected', // Set the "active" class.
+			header: false // Show header instead of the active item
+			});
+    	});
+		
+		jQuery(document).ready(function() {
+    		jQuery('#menu-secondary-items').tinyNav({
+			active: 'selected', // Set the "active" class.
+			header: false // Show header instead of the active item
+			});
+    	});
