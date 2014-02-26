3bf4a72d20726233cef099f579322cf3f5c90642
Antônio Roberto Silva <forevertonny@gmail.com>
2014-02-25 21:53:19 -0300

    added theme read v1

---
+++ wp-content/themes/read-v1-4/footer-widgets.php
@@ -0,0 +1,35 @@
+<!-- .widget-area -->
+<div class="widget-area span3" role="complementary">
+	<?php
+		if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'footer_1' ) ) :
+		endif;
+	?>
+</div>
+<!-- end .widget-area -->
+
+<!-- .widget-area -->
+<div class="widget-area span3" role="complementary">
+	<?php
+		if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'footer_2' ) ) :
+		endif;
+	?>
+</div>
+<!-- end .widget-area -->
+
+<!-- .widget-area -->
+<div class="widget-area span3" role="complementary">
+	<?php
+		if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'footer_3' ) ) :
+		endif;
+	?>
+</div>
+<!-- end .widget-area -->
+
+<!-- .widget-area -->
+<div class="widget-area span3" role="complementary">
+	<?php
+		if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'footer_4' ) ) :
+		endif;
+	?>
+</div>
+<!-- end .widget-area -->
