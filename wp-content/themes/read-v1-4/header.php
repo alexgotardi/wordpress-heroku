2447ae1f4b251fe81916c029dea7339919b949be
Antônio Roberto Silva <forevertonny@gmail.com>
2014-05-26 17:13:14 -0300

    fav icon

--- wp-content/themes/read-v1-4/header.php
+++ wp-content/themes/read-v1-4/header.php
@@ -5,21 +5,22 @@
 	<meta charset="<?php echo get_bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" type="image/x-icon">
+
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
-    
+
     <!--[if lte IE 9]>
         <script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js"></script>
         <script src="<?php echo get_template_directory_uri(); ?>/js/selectivizr-min.js"></script>
     <![endif]-->
-	
+
 	<?php
 		if ( is_singular() && get_option( 'thread_comments' ) )
 		{
 			wp_enqueue_script( 'comment-reply' );
 		}
 	?>
-	
+
 	<?php
 		wp_head();
 	?>
@@ -27,7 +28,7 @@
 
 <body <?php body_class(); ?>>
 
-    <div id="page" class="hfeed site"> 
+    <div id="page" class="hfeed site">
         <header class="site-header wrapper" role="banner">
 			<div class="row">
 			    <!-- .site-title -->
@@ -35,11 +36,11 @@
 					<h1 class="site-title">
 						<?php
 							$logo_type = get_option( 'logo_type', 'Text Logo' );
-							
+
 							if ( $logo_type == 'Text Logo' )
 							{
 								$select_text_logo = get_option( 'select_text_logo', 'WordPress Site Title' );
-								
+
 								if ( $select_text_logo == 'WordPress Site Title' )
 								{
 									$text_logo_out = get_bloginfo( 'name' );
@@ -49,7 +50,7 @@
 									$text_logo_out = stripcslashes( get_option( 'theme_site_title', "" ) );
 								}
 								// end if
-								
+
 								?>
 									<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo $text_logo_out; ?></a>
 								<?php
@@ -57,7 +58,7 @@
 							else
 							{
 								$logo_image = get_option( 'logo_image', "" );
-								
+
 								?>
 									<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>">
 										<img alt="<?php bloginfo( 'name' ); ?>" src="<?php echo $logo_image; ?>">
@@ -67,11 +68,11 @@
 							// end if
 						?>
 					</h1>
-					
+
 					<h2 class="site-description">
 						<?php
 							$select_tagline = get_option( 'select_tagline', 'WordPress Tagline' );
-							
+
 							if ( $select_tagline == 'WordPress Tagline' )
 							{
 								$tagline_out = get_bloginfo( 'description' );
@@ -81,13 +82,13 @@
 								$tagline_out = stripcslashes( get_option( 'theme_tagline', "" ) );
 							}
 							// end if
-							
+
 							echo $tagline_out;
 						?>
 					</h2>
 			    </hgroup>
 			    <!-- end .site-title -->
-				
+
 			    <nav id="site-navigation" class="main-navigation" role="navigation">
 					<?php
 						wp_nav_menu( array( 'menu' => 'top_menu',
@@ -97,23 +98,23 @@
 											'container' => false,
 											'depth' => 0,
 											'fallback_cb' => 'wp_page_menu2' ) );
-											
-											
+
+
 						$nav_menu_search = get_option( 'nav_menu_search', 'No' );
-						
+
 						if ( $nav_menu_search == 'Yes' )
 						{
 							?>
 								<script>
-								
+
 									var nav_menu_search = '<li class="nav-menu-search"><form id="search-form" role="search" method="get" action="<?php echo home_url( "/" ); ?>"><label class="screen-reader-text" for="search"><?php echo __( "Search", "read" ); ?></label><input type="text" id="search" name="s" title="<?php echo __( "Enter keyword", "read" ); ?>" value=""><input type="submit" id="search-submit" title="<?php echo __( "Search it", "read" ); ?>" value="<?php echo __( "&#8594;", "read" ); ?>"></form></li>';
-									
+
 									( function( $ )
 									{
 										$( '#site-navigation > ul' ).append( nav_menu_search );
-										
+
 									})( jQuery );
-									
+
 								</script>
 							<?php
 						}
@@ -125,12 +126,12 @@
 			<!-- end .row -->
         </header>
         <!-- end .site-header -->
-		
+
 		<?php
 			if ( is_home() || is_archive() )
 			{
 				$blog_type = get_option( 'blog_type', 'Sidebar' );
-				
+
 				if ( $blog_type == 'Sidebar' )
 				{
 					$blog_type_out = 'blog-with-sidebar';
@@ -146,6 +147,6 @@
 			}
 			// end if
 		?>
-		
+
         <section id="main" class="middle wrapper">
 			<div class="row row-fluid <?php echo $blog_type_out; ?>">
